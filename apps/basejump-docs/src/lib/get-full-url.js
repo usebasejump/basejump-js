a8b0462cf936c690df44bd1d8c9ca7810040b315
tiniscule <114822071+tiniscule@users.noreply.github.com>
2023-12-08 21:40:00 -0700

    remove URL env

--- apps/basejump-docs/src/lib/get-full-url.js
+++ apps/basejump-docs/src/lib/get-full-url.js
@@ -1,6 +1,6 @@
 export default function getFullUrl(path) {
     if (path.startsWith("http")) return path;
     const baseUrl =
-        process.env.URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || window.location.origin;
+        process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || window.location.origin;
     return [baseUrl, path?.replace(/^\//, "")].filter(Boolean).join("/");
 }
